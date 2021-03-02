9a80b0a2e7aad32204f50b71ca885852d0746fe2
Sophia Eady <s.eady@outlook.com>
2021-03-02 14:50:39 +0000

    added basic styling, imported font, changed background and edited navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :books
   resources :films
   resources :users, only: [:show, :edit, :update]
+  get '/about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
