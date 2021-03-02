8451e831f5112886ef1876cd96fdb7ae66c32365
Sophia Eady <s.eady@outlook.com>
2021-03-02 14:12:08 +0000

    controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :books
+  resources :films
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
