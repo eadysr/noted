23f231d30e9b7dc89e5874610304e14a5b30a8ee
Sophia Eady <s.eady@outlook.com>
2021-03-03 10:54:24 +0000

    basic methods worked out, fixing date problem in simpleform

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,39 @@ class BooksController < ApplicationController
     @books = Book.where(user_id: current_user.id)
   end
 
-  def show
+  def new
+    @user = User.find(params[:user_id])
+    @book = Book.new
+  end
+
+  def create
+    @user = current_user
+
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to user_books_path(current_user)
+    else
+      raise
+      render :new
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to user_books_path(current_user)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :description, :user_id)
+  end
+
+  def date_params
+    year = params[:book]['read_date(1i)'].to_i,
+    month = params[:book]['read_date(2i)'].to_i,
+    day = params[:book]['read_date(3i)'].to_i
+    read_date = day -- month -- year
   end
 end
