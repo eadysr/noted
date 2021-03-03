c3c7dbddef1457d44a08aa97c5ca436e3b5ccbcd
Sophia Eady <s.eady@outlook.com>
2021-03-03 00:47:24 +0000

    user page basic template complete, plus link to book list

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,8 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.where(user_id: current_user.id)
+  end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only: [:show, :edit, :update] do
-    resources :books, only: [:show, :new, :create, :edit, :update, :delete]
+    resources :books, only: [:index, :show, :new, :create, :edit, :update, :delete]
     member do
       get :list
     end
