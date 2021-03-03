9288858b21a09a76a6517d34c01ce855c5f8262f
Sophia Eady <s.eady@outlook.com>
2021-03-03 00:17:19 +0000

    references between user and book created in db migrations. Routes and models modified to reflect changes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_140747) do
+ActiveRecord::Schema.define(version: 2021_03_03_001056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2021_03_02_140747) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.date "read_date"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "films", force: :cascade do |t|
@@ -41,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_03_02_140747) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "users"
 end
