9e190ffecfef51bfc52a279ee0026b8267fcc769
Sophia Eady <s.eady@outlook.com>
2021-03-05 18:29:18 +0000

    added delete method and link for it in books index

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only: [:show, :edit, :update] do
-    resources :books, only: [:index, :new, :create, :delete]
+    resources :books, only: [:index, :new, :create]
     member do
       get :list
     end
   end
+  resources :books, only: [:destroy]
   get '/about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
